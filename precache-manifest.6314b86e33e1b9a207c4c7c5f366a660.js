2c72a56202a73e7e19db20ae385785643546224d
maksymkoval <makskoval714@gmail.com>
2020-06-12 19:40:17 +0300

    Updates

---
+++ precache-manifest.6314b86e33e1b9a207c4c7c5f366a660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ed2d4acfa7ae656617aa0cf100193f",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "6ff8631cadb15131597c",
+    "url": "/react-movies/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1c779641c0993cdb8204",
+    "url": "/react-movies/static/css/main.cab8b1b3.chunk.css"
+  },
+  {
+    "revision": "6ff8631cadb15131597c",
+    "url": "/react-movies/static/js/2.19e12099.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies/static/js/2.19e12099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c779641c0993cdb8204",
+    "url": "/react-movies/static/js/main.ca31ea23.chunk.js"
+  },
+  {
+    "revision": "5d3ade7f761ad408ff6c",
+    "url": "/react-movies/static/js/runtime-main.7ac388d1.js"
+  }
+]);
